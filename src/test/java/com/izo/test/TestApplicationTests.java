d8181e9c56db1826086e3f4cc5c2ba4ea2771b42
SDM2552 <se299792458@gmail.com>
2024-06-05 11:00:39 +0900

    config6

--- src/test/java/com/izo/test/TestApplicationTests.java
+++ src/test/java/com/izo/test/TestApplicationTests.java
@@ -1,15 +1,11 @@
 package com.izo.test;
 
-import com.izo.test.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class TestApplicationTests {
 
-    @MockBean
-    private ProductRepository productRepository;
     @Test
     void contextLoads() {
     }
