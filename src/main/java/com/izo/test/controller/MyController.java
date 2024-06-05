82f41b8983541e69766ee766a549a4416a4b0f6e
SDM2552 <se299792458@gmail.com>
2024-06-05 10:59:44 +0900

    config6

--- src/main/java/com/izo/test/controller/MyController.java
+++ src/main/java/com/izo/test/controller/MyController.java
@@ -1,7 +1,7 @@
 package com.izo.test.controller;
 
-import com.izo.test.entity.Product;
-import com.izo.test.service.ProductService;
+//import com.izo.test.entity.Product;
+//import com.izo.test.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,17 +12,17 @@ import java.util.List;
 @Controller
 public class MyController {
 
-    @Autowired
-    ProductService productService;
+//    @Autowired
+//    ProductService productService;
 
     @GetMapping("/")
     public String root(){
         return "root";
     }
-    @GetMapping("/products")
-    public String getAllProducts(Model model) {
-        List<Product> products = productService.findAll();
-        model.addAttribute("products", products);
-        return "products";
-    }
+//    @GetMapping("/products")
+//    public String getAllProducts(Model model) {
+//        List<Product> products = productService.findAll();
+//        model.addAttribute("products", products);
+//        return "products";
+//    }
 }
--- src/main/java/com/izo/test/repository/ProductRepository.java
+++ src/main/java/com/izo/test/repository/ProductRepository.java
@@ -1,8 +1,8 @@
-package com.izo.test.repository;
-
-import com.izo.test.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-}
+//package com.izo.test.repository;
+//
+//import com.izo.test.entity.Product;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//public interface ProductRepository extends JpaRepository<Product, Integer> {
+//}
